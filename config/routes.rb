8802ccac2c9253e950ee00121ae9963d706e6311
sunxiaomi <sunhemin@outlook.com>
2017-01-04 16:49:14 +0800

    implement my groups

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :groups do
     member do
       post :join
@@ -9,6 +8,12 @@ Rails.application.routes.draw do
 
     resources :posts
   end
-  
+
+  namespace :account do
+    resources :groups
+  end
+
+    # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
   root 'groups#index'
 end
